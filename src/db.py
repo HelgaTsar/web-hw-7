c054649ab19c9d4d2c404fc6a9147cfa6395f47b
Olha <142577158+Helga735@users.noreply.github.com>
2024-02-16 09:31:45 +0200

    hw

---
+++ src/db.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+url = 'sqlite:///hw.db'
+Base = declarative_base()
+engine = create_engine(url, echo=False, pool_size=5)
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
